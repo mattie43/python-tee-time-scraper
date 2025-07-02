ee567d61c60325785c27fe89596ec624be74efc1
Matt Ericksen <mattericksen93@gmail.com>
2025-07-01 23:03:13 -0400

    updated readme, tested first click

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Start of python scraper for tee times
+## Installation
+
+1. py -m venv .venv
+2. .venv/Scripts/activate
+3. pip install -r requirements.txt
+
+## Run server locally
+
+uvicorn main:app --reload --host 0.0.0.0 --port 8080
+
+## Deploying to Fly
+
+flyctl deploy
+
+## Testing script locally
+
+1. Update the `local_scrape_testing` URL to the URL you want to scrape.
+2. Install playwright: `playwright install`
+3. Run `py main.py`
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import asyncio
 
 app = FastAPI()
 
-@app.get("/testing")
-async def testing():
+@app.get("/test")
+async def test():
     return {
-        "content": "Successful GET from testing endpoint"
+        "content": "Successful GET from test endpoint"
     }
 
 @app.post("/scrape")
@@ -23,16 +23,26 @@ async def scrape(request: Request):
         await browser.close()
         return {"html": html}
 
-async def test_scrape(url):
+"""
+Local testing commands
+"""
+
+async def local_scrape_testing():
+    url = "https://pebble-creek-golf-club-v2.book.teeitup.com/?course=18302&date=2025-07-01&max=9999"
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
         await page.goto(url)
-        html = await page.content()
+
+        locator = page.get_by_role("gridcell", name="6", exact=True)
+        await locator.click()
+
+        # query = await page.query_selector(".MuiPickersDay-root")
+        # print(element)
+        # await page.get_by_role("button", name="6").click()
         await page.pause()
-        await browser.close()
-        return html
+        # await browser.close()
 
 # Test run
 if __name__ == "__main__":
-    asyncio.run(test_scrape("https://example.com"))
+    asyncio.run(local_scrape_testing())
